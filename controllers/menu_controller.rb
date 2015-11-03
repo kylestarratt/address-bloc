6ced94c973757fb6f8f51114cafab93b156a0a4a
Kyle Starratt <kyle@rentitez.com>
2015-11-03 10:55:28 -0700

    cp-20-complete

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -1,26 +1,26 @@
 require_relative '../models/address_book'
 
- class MenuController
-   attr_accessor :address_book
+class MenuController
+  attr_accessor :address_book
 
-   def initialize
-     @address_book = AddressBook.new
-   end
+  def initialize
+    @address_book = AddressBook.new
+  end
 
-   def main_menu
+  def main_menu
 
-     puts "Main Menu - #{@address_book.entries.count} entries"
-     puts "1 - View all entries"
-     puts "2 - Create an entry"
-     puts "3 - Search for an entry"
-     puts "4 - Import entries from a CSV"
-     puts "5 - Exit"
-     print "Enter your selection: "
+    puts "Main Menu - #{@address_book.entries.count} entries"
+    puts "1 - View all entries"
+    puts "2 - Create an entry"
+    puts "3 - Search for an entry"
+    puts "4 - Import entries from a CSV"
+    puts "5 - Exit"
+    print "Enter your selection: "
 
 
-     selection = gets.to_i
+    selection = gets.to_i
 
-     case selection
+    case selection
     when 1
       system "clear"
       view_all_entries
@@ -51,30 +51,30 @@ require_relative '../models/address_book'
 
   def view_all_entries
     @address_book.entries.each do |entry|
-     system "clear"
-     puts entry.to_s
-       entry_submenu(entry)
-     end
+      system "clear"
+      puts entry.to_s
+      entry_submenu(entry)
+    end
 
-     system "clear"
-     puts "End of entries"
+    system "clear"
+    puts "End of entries"
   end
 
   def create_entry
     system "clear"
-     puts "New AddressBloc Entry"
+    puts "New AddressBloc Entry"
 
-     print "Name: "
-     name = gets.chomp
-     print "Phone number: "
-     phone = gets.chomp
-     print "Email: "
-     email = gets.chomp
+    print "Name: "
+    name = gets.chomp
+    print "Phone number: "
+    phone = gets.chomp
+    print "Email: "
+    email = gets.chomp
 
-     @address_book.add_entry(name, phone, email)
+    @address_book.add_entry(name, phone, email)
 
-     system "clear"
-     puts "New entry created"
+    system "clear"
+    puts "New entry created"
   end
 
   def search_entries
@@ -82,27 +82,27 @@ require_relative '../models/address_book'
 
   def read_csv
   end
- end
- def entry_submenu(entry)
 
-     puts "n - next entry"
-     puts "d - delete entry"
-     puts "e - edit this entry"
-     puts "m - return to main menu"
+  def entry_submenu(entry)
 
-     selection = gets.chomp
+    puts "n - next entry"
+    puts "d - delete entry"
+    puts "e - edit this entry"
+    puts "m - return to main menu"
 
-     case selection
-     when "n"
-     when "d"
-     when "e"
-     when "m"
-       system "clear"
-       main_menu
-     else
-       system "clear"
-       puts "#{selection} is not a valid input"
-       entries_submenu(entry)
-     end
-   end
+    selection = gets.chomp
+
+    case selection
+    when "n"
+    when "d"
+    when "e"
+    when "m"
+      system "clear"
+      main_menu
+    else
+      system "clear"
+      puts "#{selection} is not a valid input"
+      entries_submenu(entry)
+    end
+  end
 end
