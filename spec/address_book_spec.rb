aee781d1b5e23dc72e1742d825a5336eff1ae582
Kyle Starratt <kyle@rentitez.com>
2015-11-04 10:19:00 -0700

    Assigntment 21 complete

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe AddressBook do
     it "should respond to entries" do
       expect(book).to respond_to(:entries)
     end
-    
+
     it "should initialize entries as an array" do
       expect(book.entries).to be_a(Array)
     end
@@ -78,4 +78,31 @@ RSpec.describe AddressBook do
      end
 
     end
+
+    describe "#import_from_csv_2" do
+      it "tests the csv import process" do
+        book.import_from_csv("entries_2.csv")
+        book_size = book.entries.size
+        expect(book_size).to eql 3
+      end
+
+      it "imports the 1st entry" do
+        book.import_from_csv("entries_2.csv")
+        entry_one = book.entries[0]
+        check_entry(entry_one, "Bill", "555-555-4854", "bill@blocmail.com")
+      end
+
+      it "imports the 2nd entry" do
+       book.import_from_csv("entries_2.csv")
+       entry_two = book.entries[1]
+       check_entry(entry_two, "Bob", "555-555-5415", "bob@blocmail.com")
+     end
+
+     it "imports the 3rd entry" do
+       book.import_from_csv("entries_2.csv")
+       entry_three = book.entries[2]
+       check_entry(entry_three, "Joe", "555-555-3660", "joe@blocmail.com")
+     end
+   end
+
 end
